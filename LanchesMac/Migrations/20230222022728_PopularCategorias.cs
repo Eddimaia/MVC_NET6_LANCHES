68816f7b5de82ca04148427798f87db7fac9d2fb
Edson Maia da Costa Moura <edson.maia@fadami.com.br>
2023-02-21 23:45:05 -0300

    Population Migration

---
+++ LanchesMac/Migrations/20230222022728_PopularCategorias.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO CATEGORIAS(CATEGORIA_NOME, DESCRICAO)
+                                    VALUES('Normal', 'Lanches feitos com ingredientes normais')");
+
+            migrationBuilder.Sql(@"INSERT INTO CATEGORIAS(CATEGORIA_NOME, DESCRICAO)
+                                    VALUES('Vegano', 'Lanches feitos com ingredientes veganos')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM CATEGORIAS");
+        }
+    }
+}
---
+++ LanchesMac/Migrations/20230222023348_PopularLanches.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularLanches : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO LANCHES ( 
+                                    NOME
+                                    ,  DESCRICAO_CURTA
+                                    ,  DESCRICAO_DETALHADA
+                                    ,  PRECO
+                                    ,  IMAGEM_URL
+                                    ,  IMAGEM_THUMB
+                                    ,  PREFERIDO
+                                    ,  ESTOQUE
+                                    ,  ID_CATEGORIA)
+                                        VALUES('Boladão', 'Hamburguer do tipo X-Salada', 'Hamburguer do tipo X-Salada', 10.99, NULL, NULL, 0, 1, 1)");
+            migrationBuilder.Sql(@"INSERT INTO LANCHES ( 
+                                    NOME
+                                    ,  DESCRICAO_CURTA
+                                    ,  DESCRICAO_DETALHADA
+                                    ,  PRECO
+                                    ,  IMAGEM_URL
+                                    ,  IMAGEM_THUMB
+                                    ,  PREFERIDO
+                                    ,  ESTOQUE
+                                    ,  ID_CATEGORIA)
+                                        VALUES('Maneirão', 'Hamburguer do tipo X-Salada Vegano', 'Hamburguer do tipo X-Salada Vegano', 11.99, NULL, NULL, 0, 1, 2)");
+            migrationBuilder.Sql(@"INSERT INTO LANCHES ( 
+                                    NOME
+                                    ,  DESCRICAO_CURTA
+                                    ,  DESCRICAO_DETALHADA
+                                    ,  PRECO
+                                    ,  IMAGEM_URL
+                                    ,  IMAGEM_THUMB
+                                    ,  PREFERIDO
+                                    ,  ESTOQUE
+                                    ,  ID_CATEGORIA)
+                                        VALUES('O Melhor', 'Hamburguer do tipo X-Burguer', 'Hamburguer do tipo X-Burguer', 9.99, NULL, NULL, 0, 1, 1)");
+            migrationBuilder.Sql(@"INSERT INTO LANCHES ( 
+                                    NOME
+                                    ,  DESCRICAO_CURTA
+                                    ,  DESCRICAO_DETALHADA
+                                    ,  PRECO
+                                    ,  IMAGEM_URL
+                                    ,  IMAGEM_THUMB
+                                    ,  PREFERIDO
+                                    ,  ESTOQUE
+                                    ,  ID_CATEGORIA)
+                                        VALUES('Sem Erro', 'Hamburguer do tipo X-Burguer com queijo', 'Hamburguer do tipo X-Burguer com queijo, cebola e picles de pepino', 10.99, NULL, NULL, 0, 1, 1)");
+            migrationBuilder.Sql(@"INSERT INTO LANCHES ( 
+                                    NOME
+                                    ,  DESCRICAO_CURTA
+                                    ,  DESCRICAO_DETALHADA
+                                    ,  PRECO
+                                    ,  IMAGEM_URL
+                                    ,  IMAGEM_THUMB
+                                    ,  PREFERIDO
+                                    ,  ESTOQUE
+                                    ,  ID_CATEGORIA)
+                                        VALUES('De Itália', 'Pizza Marguerita', 'Pizza Marguerita a moda italiana', 27.00, NULL, NULL, 1, 1, 1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM LANCHES");
+        }
+    }
+}
