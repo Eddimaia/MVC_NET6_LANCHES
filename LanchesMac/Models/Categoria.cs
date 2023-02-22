95df4da9384cd7484b843860f8b3146d045efd8e
Edson Maia da Costa Moura <edson.maia@fadami.com.br>
2023-02-21 22:37:26 -0300

    Anotations & Validations

--- LanchesMac/Models/Categoria.cs
+++ LanchesMac/Models/Categoria.cs
@@ -1,8 +1,23 @@
-﻿namespace LanchesMac.Models;
-    public class Categoria
-    {
-        public int CategoriaId { get; set; }
-        public string CategoriaNome { get;set; }
-        public string Descricao { get; set; }
-        public List<Lanche> Lanches { get; set; }
-    }
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models;
+
+[Table("Categorias")]
+public class Categoria
+{
+    [Key]
+    [Column("ID_CATEGORIA")]
+    public int IdCategoria { get; set; }
+    [Required(ErrorMessage = "Informe o nome da categoria")]
+    [Display(Name = "Nome")]
+    [Column("CATEGORIA_NOME")]
+    [StringLength(100, ErrorMessage = "O tamanho máximo é de {0} caracteres")]
+    public string CategoriaNome { get; set; }
+    [Required(ErrorMessage = "Informe a descrição da categoria")]
+    [Display(Name = "Descrição")]
+    [Column("DESCRICAO")]
+    [StringLength(200, ErrorMessage = "O tamanho máximo é de {0} caracteres")]
+    public string Descricao { get; set; } 
+    public List<Lanche> Lanches { get; set; }
+}
