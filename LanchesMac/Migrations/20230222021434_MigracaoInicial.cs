d76af3399427a69a207cfc89496cef6af95da3fb
Edson Maia da Costa Moura <edson.maia@fadami.com.br>
2023-02-21 23:16:30 -0300

    Database Creation - MigrationInicial

---
+++ LanchesMac/Migrations/20230222021434_MigracaoInicial.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracaoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    ID_CATEGORIA = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CATEGORIA_NOME = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DESCRICAO = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.ID_CATEGORIA);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lanches",
+                columns: table => new
+                {
+                    ID_LANCHE = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NOME = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    DESCRICAO_CURTA = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    DESCRICAO_DETALHADA = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    PRECO = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    IMAGEM_URL = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    IMAGEM_THUMB = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    PREFERIDO = table.Column<bool>(type: "bit", nullable: false),
+                    ESTOQUE = table.Column<bool>(type: "bit", nullable: false),
+                    ID_CATEGORIA = table.Column<int>(type: "int", nullable: false),
+                    CategoriaIdCategoria = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lanches", x => x.ID_LANCHE);
+                    table.ForeignKey(
+                        name: "FK_Lanches_Categorias_CategoriaIdCategoria",
+                        column: x => x.CategoriaIdCategoria,
+                        principalTable: "Categorias",
+                        principalColumn: "ID_CATEGORIA");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lanches_CategoriaIdCategoria",
+                table: "Lanches",
+                column: "CategoriaIdCategoria");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lanches");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
--- LanchesMac/Models/Lanche.cs
+++ LanchesMac/Models/Lanche.cs
@@ -11,32 +11,40 @@ public class Lanche
     [Required(ErrorMessage = "O {0} do lanche deve ser informado.")]
     [Display(Name = "Nome do Lanche")]
     [StringLength(80, MinimumLength = 10, ErrorMessage = "Tamanho mínimo")]
+    [Column("NOME")]
     public string Nome { get; set; }
     [Required(ErrorMessage = "A descrição deve ser informada.")]
     [Display(Name = "Descrição do Lanche")]
     [StringLength(200, MinimumLength = 20, ErrorMessage = "Descrição deve conter no mínimo {2} e no máximo {1} caracteres.")]
+    [Column("DESCRICAO_CURTA")]
     public string DescricaoCurta { get; set; }
     [Required(ErrorMessage = "A descrição deve ser informada.")]
     [Display(Name = "Descrição do Lanche")]
     [StringLength(200, MinimumLength = 20, ErrorMessage = "Descrição deve conter no mínimo {2} e no máximo {1} caracteres.")]
+    [Column("DESCRICAO_DETALHADA")]
     public string DescricaoDetalhada { get; set; }
     [Required(ErrorMessage = "Informe o preço do lanche")]
     [Display(Name = "Preço")]
-    [Column(TypeName = "dacimal(10,2)")]
+    [Column("PRECO",TypeName = "decimal(10,2)")]
     [Range(1, 999.99, ErrorMessage = "O preço do lanche deve estar ente {1} e {2}")]
     public decimal Preco { get; set; }
     [Display(Name = "Caminho Imagem Normal")]
     [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
+    [Column("IMAGEM_URL")]
     public string ImagemUrl { get; set; }
 
     [Display(Name = "Caminho Imagem Miniatura")]
     [StringLength(200, ErrorMessage = "O {0} deve ter no máximo {1} caracteres")]
+    [Column("IMAGEM_THUMB")]
     public string ImagemThumbnailUrl { get; set; }
     [Display(Name = "Preferido?")]
+    [Column("PREFERIDO")]
     public bool IsLanchePreferido { get; set; }
     [Display(Name = "Estoque?")]
+    [Column("ESTOQUE")]
     public bool EmEstoque { get; set; }
 
+    [Column("ID_CATEGORIA")]
     public int IdCategoria { get; set; }
     public virtual Categoria Categoria { get; set; }
 }
