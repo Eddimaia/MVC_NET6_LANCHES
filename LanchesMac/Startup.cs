7fa90425fa518645cc6bc2730903955feb939a2d
Edson Maia da Costa Moura <edson.maia@fadami.com.br>
2023-02-27 00:09:14 -0300

    CarrinhoCompra Instance and CarrinhoCompraViewModel

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Context;
+using LanchesMac.Models;
 using LanchesMac.Repositories;
 using LanchesMac.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -19,10 +20,10 @@ public class Startup
         //Registrando o serviço que conecta ao banco
         services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-        services.AddTransient<ICategoriaRepository, CategoriaRepository>();
         services.AddTransient<ILancheRepository, LancheRepository>();
-
+        services.AddTransient<ICategoriaRepository, CategoriaRepository>();
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
 
         services.AddControllersWithViews();
         services.AddMemoryCache();
---
+++ LanchesMac/ViewModels/CarrinhoCompraViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace LanchesMac.ViewModels;
+public class CarrinhoCompraViewModel
+{
+    public CarrinhoCompraViewModel CarrinhoCompra { get; set; }
+    public decimal CarrinhoCompraTotal { get; set; }
+}
